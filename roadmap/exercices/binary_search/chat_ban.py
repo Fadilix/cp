d5ced93a5e48d450a0d3545a69088dec481fc1d8
Fadilix <iamfadile2@gmail.com>
2026-02-27 17:03:19 +0100

    solved 1612C - Chat Ban

--- roadmap/exercices/binary_search/chat_ban.py
+++ roadmap/exercices/binary_search/chat_ban.py
@@ -1,12 +1,32 @@
 import sys
 
 
+def get_total(m, k):
+    if m <= k:
+        return m * (m + 1) // 2
+
+    left = k * (k + 1) // 2
+    rows = m - k
+    remaining = k - 1 - rows
+    right = ((k - 1) * k // 2) - (remaining * (remaining + 1) // 2)
+
+    return left + right
+
 def solve():
     k, x = map(int, sys.stdin.readline().split())
-    lo, hi = 1, 2 * k - 1
 
+    low, high = 1, 2 * k - 1
+
+    ans = high
+    while low <= high:
+        mid = (low + high) // 2
+        if get_total(mid, k) >= x:
+            high = mid - 1
+            ans = mid
+        else:
+            low = mid + 1
+    print(ans)
 
 t = int(sys.stdin.readline())
 for _ in range(t):
-
     solve()
